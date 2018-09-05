a6333a3a16291803e2a0b40114f727c93cefb279
Chaithanya Morreddi <chaitanya.reddy@anthem.com>
2018-09-05 18:17:19 -0400

    Initial Commit

---
+++ src/main/java/com/ascent/aptmanagement/AptManagementApplication.java
@@ -0,0 +1,12 @@
+package com.ascent.aptmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptManagementApplication.class, args);
+	}
+}
